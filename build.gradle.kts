38fcde25b3fa9e905f8fbc3f30c3b7f087fd96ff
Dylan Tangredi <dylan.tangredi@bvtech.com>
2025-02-11 12:03:02 +0100

    feat(oi-openapi): [CHK-3687] add OneIdentity integration
    import and convert openapi json from one identity; add generator task to build.gradle file; refactor common properties; adapt sourceSets and KotlinCompile task; add temporary validation skip;

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,16 @@ version = "0.1.0"
 
 description = "pagopa-checkout-auth-service"
 
-sourceSets { main { java { srcDirs("${layout.buildDirectory}/generated/src/main/java") } } }
+sourceSets {
+  main {
+    java {
+      srcDirs(
+        "${layout.buildDirectory}/generated/src/main/java",
+        "${layout.buildDirectory}/generated-oneidentity/src/main/java",
+      )
+    }
+  }
+}
 
 java { toolchain { languageVersion = JavaLanguageVersion.of(21) } }
 
@@ -106,37 +115,54 @@ configure<com.diffplug.gradle.spotless.SpotlessExtension> {
   }
 }
 
+val commonOpenApiConfig =
+  mapOf(
+    "swaggerAnnotations" to "false",
+    "openApiNullable" to "true",
+    "interfaceOnly" to "true",
+    "hideGenerationTimestamp" to "true",
+    "skipDefaultInterface" to "false",
+    "useSwaggerUI" to "false",
+    "reactive" to "true",
+    "useSpringBoot3" to "true",
+    "oas3" to "true",
+    "generateSupportingFiles" to "true",
+    "enumPropertyNaming" to "MACRO_CASE",
+  )
+
 tasks.register<org.openapitools.generator.gradle.plugin.tasks.GenerateTask>("auth-v1") {
   generatorName.set("spring")
   inputSpec.set("$rootDir/api-spec/v1/openapi.yaml")
   outputDir.set("$buildDir/generated")
   apiPackage.set("it.pagopa.generated.checkout.authservice")
   modelPackage.set("it.pagopa.generated.checkout.authservice")
-  generateApiTests.set(false)
   generateApiDocumentation.set(false)
   generateApiTests.set(false)
   generateModelTests.set(false)
   library.set("spring-boot")
   modelNameSuffix.set("Dto")
-  configOptions.set(
-    mapOf(
-      "swaggerAnnotations" to "false",
-      "openApiNullable" to "true",
-      "interfaceOnly" to "true",
-      "hideGenerationTimestamp" to "true",
-      "skipDefaultInterface" to "true",
-      "useSwaggerUI" to "false",
-      "reactive" to "true",
-      "useSpringBoot3" to "true",
-      "oas3" to "true",
-      "generateSupportingFiles" to "true",
-      "enumPropertyNaming" to "MACRO_CASE",
-    )
-  )
+  configOptions.set(commonOpenApiConfig)
+}
+
+tasks.register<org.openapitools.generator.gradle.plugin.tasks.GenerateTask>("oneidentity") {
+  description = "Generates the OneIdentity classes for this project."
+  group = JavaBasePlugin.DOCUMENTATION_GROUP
+  generatorName.set("spring")
+  inputSpec.set("$rootDir/api-spec/oneidentity/openapi.yaml")
+  outputDir.set("$buildDir/generated-oneidentity")
+  apiPackage.set("it.pagopa.generated.checkout.oneidentity")
+  modelPackage.set("it.pagopa.generated.checkout.oneidentity")
+  generateApiDocumentation.set(false)
+  generateApiTests.set(false)
+  generateModelTests.set(false)
+  library.set("spring-boot")
+  modelNameSuffix.set("Dto")
+  configOptions.set(commonOpenApiConfig)
+  validateSpec.set(false) // TEMP, TO REMOVE
 }
 
 tasks.withType<KotlinCompile> {
-  dependsOn("auth-v1")
+  dependsOn("auth-v1", "oneidentity")
   kotlinOptions.jvmTarget = "21"
 }
 
