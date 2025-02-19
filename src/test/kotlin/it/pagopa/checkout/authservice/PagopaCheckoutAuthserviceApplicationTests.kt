29ac85d275b5f952da25805a74160b57d540e94f
Dylan Tangredi <dylan.tangredi@bvtech.com>
2025-02-19 11:19:15 +0100

    feat(login-api): [CHK-3720] add local env vars management
    add redirect login envs to example;
    adjust application.properties envs;
    add application.test.properties;
    adapt main test class;

--- src/test/kotlin/it/pagopa/checkout/authservice/PagopaCheckoutAuthserviceApplicationTests.kt
+++ src/test/kotlin/it/pagopa/checkout/authservice/PagopaCheckoutAuthserviceApplicationTests.kt
@@ -1,10 +1,16 @@
 package it.pagopa.checkout.authservice
 
+import kotlin.test.assertTrue
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
+@TestPropertySource(locations = ["classpath:application.test.properties"])
 class PagopaCheckoutAuthserviceApplicationTests {
 
-    @Test fun contextLoads() {}
+    @Test
+    fun contextLoads() {
+        assertTrue { true }
+    }
 }
