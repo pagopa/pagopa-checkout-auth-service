b040c622a431a30c2639c70954f6cf8f8dee7b80
Pietro Tota <pietro.tota@pagopa.it>
2025-02-27 11:00:58 +0100

    fix: minor fix for propagate exception cause

--- src/main/kotlin/it/pagopa/checkout/authservice/exception/OneIdentityServerException.kt
+++ src/main/kotlin/it/pagopa/checkout/authservice/exception/OneIdentityServerException.kt
@@ -3,12 +3,16 @@ package it.pagopa.checkout.authservice.exception
 import it.pagopa.checkout.authservice.repositories.redis.bean.oidc.OidcState
 import org.springframework.http.HttpStatus
 
-class OneIdentityServerException(message: String, private val state: OidcState) :
-    ApiError("Authentication process error for state: [$state] -> $message") {
+class OneIdentityServerException(
+    message: String,
+    private val state: OidcState,
+    override val cause: Throwable? = null,
+) : ApiError("Authentication process error for state: [$state] -> $message") {
     override fun toRestException(): RestApiException =
         RestApiException(
-            HttpStatus.INTERNAL_SERVER_ERROR,
-            "Error communicating with One identity",
-            "Cannot perform authentication process for state: [$state]",
+            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR,
+            title = "Error communicating with One identity",
+            description = "Cannot perform authentication process for state: [${state.value}]",
+            cause = cause,
         )
 }
