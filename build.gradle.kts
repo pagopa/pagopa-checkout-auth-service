d384fb7a465a9cfff889877a5bb3ef395b6b464b
Pietro Tota <115724836+pietro-tota@users.noreply.github.com>
2026-04-27 15:53:45 +0200

    chore: remove unused var

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,6 @@ springBoot {
 
 object Deps {
   const val ecsLoggingVersion = "1.5.0"
-  const val openTelemetryVersion = "1.48.0"
   const val openTelemetryInstrumentationVersion = "2.26.0-alpha"
   const val springBootVersion = "3.4.2"
   const val jsonWebTokenVersion = "0.11.5"
